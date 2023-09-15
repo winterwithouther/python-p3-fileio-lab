f9ecfb0f060aabc8cbdc4d19a87e64897bc4d7cb
Eman Gurung <egurung1117@gmail.com>
2023-09-15 06:17:03 -0400

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    write_file = open(f'{file_name}.txt', mode="w", encoding="utf-8")
+    write_file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    append_file = open(f'{file_name}.txt', mode="a", encoding="utf-8")
+    append_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    read_file = open(f'{file_name}.txt', encoding="utf-8")
+    return read_file.read()
